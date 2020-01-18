6cf4159beac56c47c5d806138329db8c4f468156
Anton Nesterov <48190253+nesterow@users.noreply.github.com>
2020-01-18 14:36:18 +0300

    Update index.ts

--- modules/example/config/index.ts
+++ modules/example/config/index.ts
@@ -1,5 +1,9 @@
 const {NODE_ENV} = process.env;
-
+/*
+Be cautious: 
+  Webpack doesn't understand ternary operators (?:). 
+  To make conditional imports work properly use IF statements. 
+*/
 export default NODE_ENV === 'development' ? require('./development').default   :
                NODE_ENV === 'test' ? require('./testing').default :
                NODE_ENV === 'production' ? require('./production').default :
